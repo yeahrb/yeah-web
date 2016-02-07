fbe8bae9587a3c4030bcf250c5f9685ffc9ee674
Artur Ostręga <artmarost@gmail.com>
2016-02-06 18:18:26 -0600

    SpriteLook animation functionality

--- opal/yeah/sprite_look.rb
+++ opal/yeah/sprite_look.rb
@@ -11,15 +11,15 @@ class Yeah::SpriteLook < Yeah::ImageLook
     end
   end
 
-  attr_reader :frame
-  attr_accessor :width, :height, :animation
+  attr_reader :animation, :frames, :frame
+  attr_accessor :width, :height
 
   def initialize
     super
 
     @width = self.class.width
     @height = self.class.height
-    @frame = 0
+    self.animation = self.class.animations.keys.first
   end
 
   def size
@@ -30,11 +30,28 @@ class Yeah::SpriteLook < Yeah::ImageLook
     self.width, self.height = value
   end
 
+  def animation=(value)
+    @frames = self.class.animations[value]
+
+    if @frames.respond_to? :keys
+      @frames = @frames[:frames]
+    end
+
+    if @frames.respond_to? :to_a
+      @frames = @frames.to_a
+    else
+      @frames = [@frames]
+    end
+
+    @frame = @frames.first
+    @animation = value
+  end
+
   def draw(thing, display)
     x = @frame * @width % @image.width
     y = (@frame * @width / @image.width).floor * @height
     display.draw_image_part(@image, thing.x, thing.y, x, y, @width, @height)
     @frame += 1
-    @frame = 0 if @frame >= (@image.width / @width) * (@image.height / @height)
+    @frame = @frames.first if @frame >= @frames.last
   end
 end
