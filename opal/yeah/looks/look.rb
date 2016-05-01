741abcfc3264626769f50ad7c23d9aec04582053
Artur Ostręga <artmarost@gmail.com>
2016-04-30 22:03:11 -0500

    Implement Look #offset(_x)(_y)[=], #x, #y, #position

--- opal/yeah/looks/look.rb
+++ opal/yeah/looks/look.rb
@@ -1,8 +1,51 @@
 class Yeah::Look
+  class << self
+    attr_accessor :offset_x, :offset_y
+
+    def offset_x
+      @offset_x ||= 0
+    end
+
+    def offset_y
+      @offset_y ||= 0
+    end
+
+    def offset
+      [@offset_x, @offset_y]
+    end
+
+    def offset=(value)
+      @offset_x, @offset_y = value
+    end
+  end
+
   attr_reader :thing
+  attr_accessor :offset_x, :offset_y
 
   def initialize(thing)
     @thing = thing
+    @offset_x = self.class.offset_x
+    @offset_y = self.class.offset_y
+  end
+
+  def x
+    @thing.x + @offset_x
+  end
+
+  def y
+    @thing.y + @offset_y
+  end
+
+  def position
+    [@thing.x + @offset_x, @thing.y + @offset_y]
+  end
+
+  def offset
+    [@offset_x, @offset_y]
+  end
+
+  def offset=(value)
+    @offset_x, @offset_y = value
   end
 
   def draw(display, elapsed)
